87fa917c155fa3a4e381de836688d226b7b4e7a5
buraksahin <buraksahin.tr@gmail.com>
2017-11-29 16:53:46 +0300

    Fix indentations

--- Preprocessors_Macros.c
+++ Preprocessors_Macros.c
@@ -10,9 +10,9 @@
 #define PRINT(x, to) while(x<=to){printf("%d ",x); x+=x;}
 #define concat(x, y) x##y
 #define PRINT(num, str) {\
-            printf("%d is", num);\
-            printf(" %s number\n", str);\
-           }
+    printf("%d is", num);\
+    printf(" %s number\n", str);\
+}
 
 int main(){
 	printf("%.2f", PI);
@@ -27,17 +27,16 @@ int main(){
 	printf("Current Date :%s\n", __DATE__ );
 	printf("Current Time :%s\n", __TIME__ );
 	printf("Line Number :%d\n", __LINE__ );
-
-  printf("PI: %f", concat(P,I));
+    printf("PI: %f", concat(P,I));
 	int num;
     printf("Enter a number: ");
     scanf("%d", &num);
     if(num & 1){
-			PRINT(num, "Odd");
-		}
+        PRINT(num, "Odd");
+    }
     else{
-			PRINT(num, "Even");
-		}
+        PRINT(num, "Even");
+    }
 
 	return 0;
 }
