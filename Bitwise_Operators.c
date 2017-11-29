69699eb9b4a780fa043136a5a140feac29dba1d6
buraksahin <buraksahin.tr@gmail.com>
2017-11-29 16:55:57 +0300

    Fix indentations

--- Bitwise_Operators.c
+++ Bitwise_Operators.c
@@ -8,10 +8,10 @@ int main(){
     printf("a&b = %d\n", a&b); 		    // AND result is 00000001
     printf("a|b = %d\n", a|b);  	    // OR result is 00001101
     printf("a^b = %d\n", a^b); 		    // XOR result is 00001100
-    printf ("3^4 = %d\n", 3^4);         // XOR (00000010) ^ (00000101) result is (00000111)
+    printf ("3^4 = %d\n", 3^4);       // XOR (00000010) ^ (00000101) result is (00000111)
     printf("~a = %d\n", a=~a);  	    // NOT result is 11111010
-    printf("b<<1 = %d\n", b<<1);        // Left Shift result is 00010010
-    printf("b>>1 = %d\n", b>>1);        // Right Shift result is 00000100
+    printf("b<<1 = %d\n", b<<1);      // Left Shift result is 00010010
+    printf("b>>1 = %d\n", b>>1);      // Right Shift result is 00000100
 
     return 0;
 }
